eaafafc438582bec1d66c281066808af63e15237
Tommy Jung <tommy.dc.jung@gmail.com>
2022-08-16 10:35:27 -0700

    add gups rmw

---
+++ apps/gups_rmw/kernel.cpp
@@ -0,0 +1,51 @@
+#include <bsg_manycore.h>
+#include <bsg_cuda_lite_barrier.h>
+
+#define MAX_UPDATES 512
+#define CONCURRENCY 4
+
+int LOCAL_X[MAX_UPDATES];
+
+extern "C" __attribute__ ((noinline))
+int
+kernel_gups_rmw(int * dram_A, int * dram_X, int num_update) {
+
+  bsg_barrier_hw_tile_group_init();
+
+  // setup
+  int start = num_update * __bsg_id;
+  for (int i = 0; i < num_update; i++) {
+    LOCAL_X[i] = dram_X[start+i];
+  }
+
+  bsg_barrier_hw_tile_group_sync();
+
+
+  // Run gups
+  bsg_cuda_print_stat_kernel_start();
+  bsg_fence();
+
+  for (int i = 0; i < num_update; i += CONCURRENCY) {
+    int reg_x[CONCURRENCY];
+    int reg_a[CONCURRENCY];
+
+    bsg_unroll(32)
+    for (int j = 0; j < CONCURRENCY; j++) {
+      reg_x[j] = LOCAL_X[i+j];
+      reg_a[j] = dram_A[reg_x[j]]; // load
+    }
+    
+    bsg_unroll(32)
+    for (int j = 0; j < CONCURRENCY; j++) {
+      dram_A[reg_x[j]] = reg_a[j] ^ reg_x[j]; // modify + store
+    }
+  }
+  
+
+  bsg_fence();
+  bsg_cuda_print_stat_kernel_end();
+  bsg_fence();
+  bsg_barrier_hw_tile_group_sync();
+
+  return 0;
+}
---
+++ apps/gups_rmw/main.c
@@ -0,0 +1,107 @@
+#include <bsg_manycore_tile.h>
+#include <bsg_manycore_errno.h>
+#include <bsg_manycore_tile.h>
+#include <bsg_manycore_loader.h>
+#include <bsg_manycore_cuda.h>
+#include <math.h>
+#include <complex.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <stdio.h>
+#include <bsg_manycore_regression.h>
+
+#define ALLOC_NAME "default_allocator"
+
+#define SIZE (1024*1024*64) // in 64M word
+#define NUM_UPDATE 512
+#define MAX_NUM_TILE 128
+
+int kernel_gups_rmw(int argc, char **argv) {
+
+  int rc;
+  char *bin_path, *test_name;
+  struct arguments_path args = {NULL, NULL};
+  
+  argp_parse(&argp_path, argc, argv, 0, 0, &args);
+  bin_path = args.path;
+  test_name = args.name;
+
+  srand(time);
+ 
+  // Initialize Device.
+  hb_mc_device_t device;
+  BSG_CUDA_CALL(hb_mc_device_init(&device, test_name, 0));
+
+  // Allocate a block of memory in host.
+  int * A_host = (int *) malloc(SIZE*sizeof(int));
+  int * X_host = (int *) malloc(NUM_UPDATE*MAX_NUM_TILE*sizeof(int));
+  for (int i = 0; i < SIZE; i++) {
+    A_host[i] = i; 
+  }
+  for (int i = 0; i < NUM_UPDATE*MAX_NUM_TILE; i++) {
+    X_host[i] = rand() % SIZE;
+  }
+
+
+  hb_mc_pod_id_t pod;
+  hb_mc_device_foreach_pod_id(&device, pod)
+  {
+    bsg_pr_info("Loading program for pod %d\n.", pod);
+    BSG_CUDA_CALL(hb_mc_device_set_default_pod(&device, pod));
+    BSG_CUDA_CALL(hb_mc_device_program_init(&device, bin_path, ALLOC_NAME, 0));
+
+
+    // Allocate a block of memory in device.
+    eva_t A_device, X_device;
+    BSG_CUDA_CALL(hb_mc_device_malloc(&device, SIZE * sizeof(int), &A_device));
+    BSG_CUDA_CALL(hb_mc_device_malloc(&device, NUM_UPDATE * MAX_NUM_TILE * sizeof(int), &X_device));
+  
+ 
+    // DMA Transfer to device.
+    hb_mc_dma_htod_t htod_job [] = {
+      {
+        .d_addr = A_device,
+        .h_addr = (void *) &A_host[0],
+        .size = SIZE * sizeof(int)
+      },
+      {
+        .d_addr = X_device,
+        .h_addr = (void *) &X_host[0],
+        .size = NUM_UPDATE * MAX_NUM_TILE * sizeof(int)
+      }
+    };
+
+    BSG_CUDA_CALL(hb_mc_device_dma_to_device(&device, htod_job, 2));
+
+
+    // CUDA arguments
+    hb_mc_dimension_t tg_dim = { .x = 16, .y = 8};
+    hb_mc_dimension_t grid_dim = { .x = 1, .y = 1};
+    #define CUDA_ARGC 3
+    uint32_t cuda_argv[CUDA_ARGC] = {A_device, X_device, NUM_UPDATE};
+    
+    // Enqueue Kernel.
+    BSG_CUDA_CALL(hb_mc_kernel_enqueue (&device, grid_dim, tg_dim, "kernel_gups_rmw", CUDA_ARGC, cuda_argv));
+    
+    // Launch kernel.
+    hb_mc_manycore_trace_enable((&device)->mc);
+    BSG_CUDA_CALL(hb_mc_device_tile_groups_execute(&device));
+    hb_mc_manycore_trace_disable((&device)->mc);
+
+    // Freeze tiles.
+    BSG_CUDA_CALL(hb_mc_device_program_finish(&device));
+  }
+
+  // Free host memory.
+  free(A_host);
+  free(X_host);
+
+  BSG_CUDA_CALL(hb_mc_device_finish(&device));
+  return HB_MC_SUCCESS; 
+}
+
+
+declare_program_main("gups_rmw", kernel_gups_rmw);
