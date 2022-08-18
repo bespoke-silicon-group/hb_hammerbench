f1db8990115f658dbf085def2fdfd22d500b78a7
Tommy Jung <tommy.dc.jung@gmail.com>
2022-08-18 10:12:11 -0700

    tweak test params

--- apps/memcpy/tests.mk
+++ apps/memcpy/tests.mk
@@ -2,7 +2,5 @@
 # CHANGE ME: TESTS				        #
 # TESTS += $(call test-name,[buffer-size],[warm-cache]) #
 #########################################################
-TESTS += $(call test-name,512,yes)
-TESTS += $(call test-name,512,no)
-TESTS += $(call test-name,262144,yes)
+TESTS += $(call test-name,131072,yes)
 TESTS += $(call test-name,262144,no)
