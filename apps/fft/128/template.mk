5f6e068a17aa0357357b1abb10d0b19c03f6a5d6
Tommy Jung <tommy.dc.jung@gmail.com>
2022-08-18 15:48:18 -0700

    refactor fft128

---
+++ apps/fft/128/template.mk
@@ -0,0 +1,110 @@
+# This Makefile compiles, links, and executes examples Run `make help`
+# to see the available targets for the selected platform.
+
+################################################################################
+# environment.mk verifies the build environment and sets the following
+# makefile variables:
+#
+# LIBRAIRES_PATH: The path to the libraries directory
+# HARDWARE_PATH: The path to the hardware directory
+# EXAMPLES_PATH: The path to the examples directory
+# BASEJUMP_STL_DIR: Path to a clone of BaseJump STL
+# BSG_MANYCORE_DIR: Path to a clone of BSG Manycore
+###############################################################################
+HB_HAMMERBENCH_PATH:=$(shell git rev-parse --show-toplevel)
+include $(HB_HAMMERBENCH_PATH)/mk/environment.mk
+
+# KERNEL_NAME is the name of the CUDA-Lite Kernel
+KERNEL_NAME = fft_128x128_all_no_twiddle
+
+###############################################################################
+# Host code compilation flags and flow
+###############################################################################
+# import parameters and APP_PATH
+include parameters.mk
+include app_path.mk
+
+# Tile Group Dimensions
+TILE_GROUP_DIM_X = 16
+TILE_GROUP_DIM_Y = 8
+
+vpath %.c		$(APP_PATH)
+vpath %.cpp $(APP_PATH)
+
+# TEST_SOURCES is a list of source files that need to be compiled
+TEST_SOURCES = main.c
+
+DEFINES += -D_XOPEN_SOURCE=500 -D_BSD_SOURCE -D_DEFAULT_SOURCE
+DEFINES += -DLOCAL_POINTS=128
+DEFINES += -DNUM_ITER=$(num-iter)
+CDEFINES +=  -Dbsg_tiles_X=$(TILE_GROUP_DIM_X) -Dbsg_tiles_Y=$(TILE_GROUP_DIM_Y)
+CXXDEFINES += 
+
+FLAGS     = -g -Wall -Wno-unused-function -Wno-unused-variable
+CFLAGS   += -std=c99 $(FLAGS)
+CXXFLAGS += -std=c++11 $(FLAGS)
+ifeq ($(warm-cache),yes)
+FLAGS += -DWARM_CACHE
+endif
+# compilation.mk defines rules for compilation of C/C++
+include $(EXAMPLES_PATH)/compilation.mk
+
+###############################################################################
+# Host code link flags and flow
+###############################################################################
+
+LDFLAGS +=
+
+# link.mk defines rules for linking of the final execution binary.
+include $(EXAMPLES_PATH)/link.mk
+
+###############################################################################
+# Device code compilation flow
+###############################################################################
+
+# BSG_MANYCORE_KERNELS is a list of manycore executables that should
+# be built before executing.
+BSG_MANYCORE_KERNELS = kernel.riscv
+
+
+kernel.rvo: $(APP_PATH)/../common/fft.hpp
+kernel.rvo: RISCV_INCLUDES += -I$(APP_PATH)/../common
+kernel.rvo: RISCV_CXX=$(RISCV_CLANGXX)
+
+kernel.riscv: kernel.rvo
+
+# Tile Group Dimensions
+RISCV_DEFINES += -Dbsg_tiles_X=$(TILE_GROUP_DIM_X)
+RISCV_DEFINES += -Dbsg_tiles_Y=$(TILE_GROUP_DIM_Y)
+RISCV_DEFINES += -DCACHE_LINE_WORDS=$(BSG_MACHINE_VCACHE_LINE_WORDS)
+ifeq ($(warm-cache),yes)
+RISCV_DEFINES += -DWARM_CACHE
+endif
+RISCV_CCXXFLAGS += -O3
+
+include $(EXAMPLES_PATH)/cuda/riscv.mk
+
+###############################################################################
+# Execution flow
+#
+# C_ARGS: Use this to pass arguments that you want to appear in argv
+#         For SPMD tests C arguments are: <Path to RISC-V Binary> <Test Name>
+#
+# SIM_ARGS: Use this to pass arguments to the simulator
+###############################################################################
+C_ARGS ?= $(BSG_MANYCORE_KERNELS) $(KERNEL_NAME)
+
+SIM_ARGS ?=
+
+# Include platform-specific execution rules
+include $(EXAMPLES_PATH)/execution.mk
+
+###############################################################################
+# Regression Flow
+###############################################################################
+
+regression: exec.log
+	@grep "BSG REGRESSION TEST .*PASSED.*" $< > /dev/null
+
+.DEFAULT_GOAL := help
+
---
+++ apps/fft/128/tests.mk
@@ -0,0 +1,3 @@
+# $(call test-name,[num-iter],[warm-cache])
+TESTS += $(call test-name,3,yes)
+TESTS += $(call test-name,8,no)
