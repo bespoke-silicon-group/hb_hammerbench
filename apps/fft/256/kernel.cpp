df29a4e81d48b75edb7705d762d41839aa8a282b
Tommy Jung <tommy.dc.jung@gmail.com>
2022-08-18 22:32:13 -0700

    256 refactor

--- apps/fft/256/kernel.cpp
+++ apps/fft/256/kernel.cpp
@@ -9,6 +9,28 @@
 #include <fft.hpp>
 
 
+// Cache warming function.
+// Can only fit A and TW in the cache.
+#ifdef WARM_CACHE
+#define CACHE_LINE_COMPLEX 8
+__attribute__ ((noinline))
+static int warmup(FP32Complex *in, FP32Complex *out, float bsg_attr_remote * bsg_attr_noalias tw, int N, int num_iter)
+{
+  for (int i = __bsg_id*CACHE_LINE_COMPLEX; i < N*num_iter; i += bsg_tiles_X*bsg_tiles_Y*CACHE_LINE_COMPLEX) {
+      asm volatile ("lw x0, %[p]" :: [p] "m" (in[i]));
+      //asm volatile ("lw x0, %[p]" :: [p] "m" (out[i]));
+  }
+  for (int i = __bsg_id*CACHE_LINE_COMPLEX; i < N; i += bsg_tiles_X*bsg_tiles_Y*CACHE_LINE_COMPLEX) {
+      asm volatile ("lw x0, %[p]" :: [p] "m" (tw[i]));
+  }
+  bsg_fence();
+  bsg_barrier_hw_tile_group_sync();
+  return 0;
+}
+#endif
+
+
+
 FP32Complex fft_workset[NUM_POINTS];
 
 extern "C" __attribute__ ((noinline))
@@ -16,40 +38,55 @@ int
 kernel_fft(FP32Complex * bsg_attr_noalias  in,
            FP32Complex * bsg_attr_noalias out,
            float bsg_attr_remote * bsg_attr_noalias tw,
-           //FP32Complex * bsg_attr_noalias tw,
-           int N) {
-    /* bsg_set_tile_x_y(); */
+           int N,
+           int num_iter
+            ) {
 
     bsg_barrier_hw_tile_group_init();
+    bsg_fence();
+
+    #ifdef WARM_CACHE
+    warmup(in, out, tw, N, num_iter);
+    #endif
+
     bsg_cuda_print_stat_kernel_start();
 
-    bsg_cuda_print_stat_start(1);
-    for (int iter = 0; iter < 2; iter++) {
-        load_fft_store_no_twiddle(in, in, fft_workset, tw, iter*128+__bsg_id, 256, 256, N, 1);
+    int stat_count = 1;
+    for (int i = 0; i < num_iter; i++) {
+      // Step 1
+      //bsg_cuda_print_stat_start(stat_count);
+      for (int iter = 0; iter < 2; iter++) {
+        load_fft_store_no_twiddle(&in[i*N], &in[i*N], fft_workset, tw, iter*128+__bsg_id, 256, 256, N, 1);
+      }
+      //bsg_cuda_print_stat_end(stat_count); stat_count++;
+      asm volatile("": : :"memory");
+      bsg_barrier_hw_tile_group_sync();
+
+      // Step 2
+      //bsg_cuda_print_stat_start(stat_count);
+      // Unroll this
+      opt_square_transpose(&in[i*N], 256);
+      //bsg_cuda_print_stat_end(stat_count); stat_count++;
+      asm volatile("": : :"memory");
+      bsg_barrier_hw_tile_group_sync();
+
+
+      // step 3
+      bsg_cuda_print_stat_start(stat_count);
+      for (int iter = 0; iter < 2; iter++) {
+        load_fft_store_no_twiddle(&in[i*N], &out[i*N], fft_workset, tw, iter*128+__bsg_id, 256, 256, N, 0);
+      }
+      bsg_cuda_print_stat_end(stat_count); stat_count++;
+      bsg_barrier_hw_tile_group_sync();
     }
-    bsg_cuda_print_stat_end(1);
 
-    asm volatile("": : :"memory");
-    bsg_barrier_hw_tile_group_sync();
 
-    bsg_cuda_print_stat_start(2);
-    // Unroll this
-    opt_square_transpose(in, 256);
-    bsg_cuda_print_stat_end(2);
-
-    asm volatile("": : :"memory");
-    bsg_barrier_hw_tile_group_sync();
-
-    bsg_cuda_print_stat_start(3);
-    for (int iter = 0; iter < 2; iter++) {
-        load_fft_store_no_twiddle(in, out, fft_workset, tw, iter*128+__bsg_id, 256, 256, N, 0);
-    }
-    bsg_cuda_print_stat_end(3);
-
+    // Kernel end
     bsg_cuda_print_stat_kernel_end();
-
-    asm volatile("": : :"memory");
+    bsg_fence();
     bsg_barrier_hw_tile_group_sync();
+
+
 
     return 0;
 }
---
+++ apps/fft/256/tests.mk
@@ -0,0 +1,3 @@
+# $(call test-name,[num-iter],[warm-cache])
+TESTS += $(call test-name,1,yes)
+TESTS += $(call test-name,2,no)
