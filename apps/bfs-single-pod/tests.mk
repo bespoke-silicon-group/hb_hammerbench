e57d0028e402a46bf1f5c7bfd0f19eb67387b800
Max Ruttenberg <mrutt@cs.washington.edu>
2022-10-01 09:02:32 -0700

    all tests

--- apps/bfs-single-pod/tests.mk
+++ apps/bfs-single-pod/tests.mk
@@ -6,5 +6,5 @@ TESTS += $(call test-name,g7k1,122)
 TESTS += $(call test-name,u7k1,99)
 TESTS += $(call test-name,g10k8,812)
 TESTS += $(call test-name,u10k8,916)
-
-
+TESTS += $(call test-name,u16k16,44337)
+TESTS += $(call test-name,g16k16,61526)
