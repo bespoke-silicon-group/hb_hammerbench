38516354cd244c675bcea04e4a7f12ab02815c2a
Max Ruttenberg <mrutt@cs.washington.edu>
2022-10-04 11:26:47 -0700

    tests.mk updates

--- apps/bfs-single-pod/tests.mk
+++ apps/bfs-single-pod/tests.mk
@@ -1,10 +1,17 @@
 ####################
 # CHANGE ME: TESTS #
 ####################
-# TESTS += $(call test-name,[input],[start])
-TESTS += $(call test-name,g7k1,122)
-TESTS += $(call test-name,u7k1,99)
-TESTS += $(call test-name,g10k8,812)
-TESTS += $(call test-name,u10k8,916)
-TESTS += $(call test-name,u16k16,44337)
-TESTS += $(call test-name,g16k16,61526)
+# TESTS += $(call test-name,[input],[start],[opt-fwd-ilp-innner],[opt-mu-ilp-inner],[opt-rev-pre-outer])
+# TESTS += $(call test-name,g7k1,122,1,1,1)
+# TESTS += $(call test-name,u7k1,99,1,1,1)
+# TESTS += $(call test-name,g10k8,812,1,1,1)
+# TESTS += $(call test-name,u10k8,916,1,1,1)
+# TESTS += $(call test-name,u16k16,44337,1,1,1)
+
+# TESTS += $(call test-name,g16k16,61526,1,1,1)
+# TESTS += $(call test-name,g16k16,61526,1,2,1)
+# TESTS += $(call test-name,g16k16,61526,1,4,1)
+
+TESTS += $(call test-name,g16k16,61526,1,2,1)
+TESTS += $(call test-name,g16k16,61526,1,2,2)
+TESTS += $(call test-name,g16k16,61526,1,2,4)
