84b9ffb41c390079ee6347d8a2d853528b73411c
Max Ruttenberg <mrutt@cs.washington.edu>
2022-10-31 14:59:19 -0700

    [jacobi] harness setup

--- apps/jacobi/tests.mk
+++ apps/jacobi/tests.mk
@@ -2,6 +2,6 @@
 # CHANGE ME: TESTS				        #
 # TESTS += $(call test-name,[nx],[ny],[nz],[num-iter],[warm-cache]) #
 #########################################################
-TESTS += $(call test-name,16,16,512,1,yes)
+#TESTS += $(call test-name,16,16,512,1,yes)
 TESTS += $(call test-name,32,16,512,1,no)
 #TESTS += $(call test-name,32,16,512,1,no)
