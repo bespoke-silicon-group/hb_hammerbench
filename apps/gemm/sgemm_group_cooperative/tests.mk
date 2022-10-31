abd7c0353d161a5d4a2dab8b2cace3785cf499d4
Max Ruttenberg <mrutt@cs.washington.edu>
2022-10-31 14:48:18 -0700

    [gemm] harness setup

--- apps/gemm/sgemm_group_cooperative/tests.mk
+++ apps/gemm/sgemm_group_cooperative/tests.mk
@@ -5,8 +5,8 @@
 #TESTS +=  $(call test-name,128,256,256,no,yes)
 
 # cache resident setting
-TESTS +=  $(call test-name,256,256,256,yes,no)
+# TESTS +=  $(call test-name,256,256,256,yes,no)
 
 # cache non resident setting
 TESTS +=  $(call test-name,512,512,512,no,yes)
-TESTS +=  $(call test-name,512,512,512,no,no)
+#TESTS +=  $(call test-name,512,512,512,no,no)
