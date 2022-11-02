71dda86c1410585cc578b13feef3e32aec1f8a67
Tommy Jung <tommy.dc.jung@gmail.com>
2022-11-02 11:44:22 -0700

    save gups

--- apps/gups_rmw/tests.mk
+++ apps/gups_rmw/tests.mk
@@ -1,8 +1,8 @@
-
-# Test name : (A-size, warm-cache)
-
-# A fits in vcache
-TESTS += $(call test-name,262144,yes)
-# Maximum DRAM size
-TESTS += $(call test-name,67108864,no)
-
+#ASIZE = 67108864
+WARMCACHE = yes
+TESTS += $(call test-name,4,2,65536,$(WARMCACHE))
+TESTS += $(call test-name,4,4,65536,$(WARMCACHE))
+TESTS += $(call test-name,8,4,131072,$(WARMCACHE))
+TESTS += $(call test-name,8,8,131072,$(WARMCACHE))
+TESTS += $(call test-name,16,8,262144,$(WARMCACHE))
+TESTS += $(call test-name,16,16,262144,$(WARMCACHE))
