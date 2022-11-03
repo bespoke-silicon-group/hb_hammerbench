a68ed65cb4c7041c4bcc29a81423dee06be249ed
Tommy Jung <tommy.dc.jung@gmail.com>
2022-11-03 15:08:28 -0700

    gups iter

--- apps/gups_rmw/tests.mk
+++ apps/gups_rmw/tests.mk
@@ -1,8 +1,9 @@
 #ASIZE = 67108864
+ASIZE = 1024
 WARMCACHE = yes
-TESTS += $(call test-name,4,2,65536,$(WARMCACHE))
-TESTS += $(call test-name,4,4,65536,$(WARMCACHE))
-TESTS += $(call test-name,8,4,131072,$(WARMCACHE))
-TESTS += $(call test-name,8,8,131072,$(WARMCACHE))
-TESTS += $(call test-name,16,8,262144,$(WARMCACHE))
-TESTS += $(call test-name,16,16,262144,$(WARMCACHE))
+TESTS += $(call test-name,4,2,$(ASIZE),$(WARMCACHE))
+TESTS += $(call test-name,4,4,$(ASIZE),$(WARMCACHE))
+TESTS += $(call test-name,8,4,$(ASIZE),$(WARMCACHE))
+TESTS += $(call test-name,8,8,$(ASIZE),$(WARMCACHE))
+TESTS += $(call test-name,16,8,$(ASIZE),$(WARMCACHE))
+TESTS += $(call test-name,16,16,$(ASIZE),$(WARMCACHE))
