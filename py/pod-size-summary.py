4a693449651f2b3f3db3776291d18470a3853023
Max Ruttenberg <mrutt@cs.washington.edu>
2022-11-12 12:17:25 -0800

    fixup again

--- py/pod-size-summary.py
+++ py/pod-size-summary.py
@@ -1,6 +1,11 @@
 import sys
 import re
-test_dirs = sys.argv[1:]
+
+order = ['16x16', '16x8', '4x2', '4x4', '8x4', '8x8']
+if sys.argv[1] == 'exclude-16x8':
+    order = ['16x16', '4x2', '4x4', '8x4', '8x8']
+
+test_dirs = sys.argv[2:]
 
 r = {}
 
@@ -18,7 +23,7 @@ for test_dir in test_dirs:
         y = re.search(r'\-y_(\d+)', test_dir).group(1)
         r['%sx%s'%(x,y)] = (instrs,cycles)
 
-order = ['16x16', '16x8', '4x2', '4x4', '8x4', '8x8']
+
 
 for pod_size in order:
     instr,cycles = r[pod_size]
