2fd162b7ca2f52203caeffdcf9c44159b7d001f9
Max Ruttenberg <mrutt@cs.washington.edu>
2022-11-13 22:59:03 -0800

    [fft/256] turn off trace

--- apps/fft/common/main.c
+++ apps/fft/common/main.c
@@ -210,9 +210,13 @@ int kernel_fft (int argc, char **argv) {
                 /*****************************************************************************************************************
                  * Launch and execute all tile groups on device and wait for all to finish.
                  ******************************************************************************************************************/
-                //hb_mc_manycore_trace_enable((&device)->mc);
+#ifdef TRACE_ENABLE
+                hb_mc_manycore_trace_enable((&device)->mc);
+#endif
                 BSG_CUDA_CALL(hb_mc_device_tile_groups_execute(&device));
-                //hb_mc_manycore_trace_disable((&device)->mc);
+#ifdef TRACE_ENABLE
+                hb_mc_manycore_trace_disable((&device)->mc);
+#endif
 
 
                 // Verify FFT results.
