129dc43cfe64228ae744c58725ff510ed79fc6ba
Max Ruttenberg <mrutt@cs.washington.edu>
2022-12-21 12:21:51 -0800

    [pod-size-stall-breakdown] script for spgemm

--- py/pod-size-stall-breakdown.py
+++ py/pod-size-stall-breakdown.py
@@ -2,14 +2,21 @@ import numpy as np
 import sys
 import re
 import pandas
+import argparse
 from vanilla_parser.stats_parser import CudaStatTag
 from itertools import product
 
+parser = argparse.ArgumentParser()
+parser.add_argument("test_dirs", nargs="+")
+parser.add_argument("--spgemm", action="store_true")
+parser.add_argument("--exclude-16x8", action="store_true")
+arguments = parser.parse_args()
+
 order = ['16x16', '16x8', '4x2', '4x4', '8x4', '8x8']
-if sys.argv[1] == 'exclude-16x8':
+if arguments.exclude_16x8:
     order = ['16x16', '4x2', '4x4', '8x4', '8x8']
 
-test_dirs = sys.argv[2:]
+test_dirs = arguments.test_dirs
 
 stall_types = [
     "stall_depend_dram_load",
@@ -66,8 +73,12 @@ def aggregate_stall_breakdown(data):
 for test_dir in test_dirs:
     data = pandas.read_csv(test_dir + '/vanilla_stats.csv')
     stalls = aggregate_stall_breakdown(data)
-    x = re.search(r'\-x_(\d+)', test_dir).group(1)
-    y = re.search(r'\-y_(\d+)', test_dir).group(1)
+    if arguments.spgemm:
+        x = re.search(r'(\d+)_tiles-x', test_dir).group(1)
+        y = re.search(r'(\d+)_tiles-y', test_dir).group(1)
+    else:
+        x = re.search(r'\-x_(\d+)', test_dir).group(1)
+        y = re.search(r'\-y_(\d+)', test_dir).group(1)
     r["%sx%s"%(x,y)]={
         stall_type : stalls[idx]
         for (idx,stall_type) in enumerate(stall_types)
