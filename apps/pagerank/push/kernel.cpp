0e96daacb657ff2d0009685be1b8d520cc38b3d2
Max Ruttenberg <mrutt@cs.washington.edu>
2023-01-12 11:10:23 -0800

    algorithm rewrite => push direction

--- apps/pagerank/push/kernel.cpp
+++ apps/pagerank/push/kernel.cpp
@@ -30,41 +30,48 @@ __attribute__((section(".dram"))) int beta_score;
 __attribute__((section(".dram"))) std::atomic<int> workq;
 
 extern "C"
-int __attribute__ ((noinline)) pagerank_push(int bsg_attr_remote * bsg_attr_noalias in_indices,
-                                             int bsg_attr_remote * bsg_attr_noalias in_neighbors,
+int __attribute__ ((noinline)) pagerank_push(int bsg_attr_remote * bsg_attr_noalias out_indices,
+                                             int bsg_attr_remote * bsg_attr_noalias out_neighbors,
                                              int bsg_attr_remote * bsg_attr_noalias out_degree,
                                              int bsg_attr_remote * bsg_attr_noalias old_rank,
                                              int bsg_attr_remote * bsg_attr_noalias new_rank,
                                              int bsg_attr_remote * bsg_attr_noalias contrib,
                                              int bsg_attr_remote * bsg_attr_noalias contrib_new,
-                                             int V) {
+                                             int V) {    
   bsg_barrier_hw_tile_group_init();
   bsg_cuda_print_stat_kernel_start();
-
+  ////////////////////////////////////
+  // update edge - push direction   //
+  ////////////////////////////////////
   int start = 0;
   int end = V;
   int length = end - start;
   for(int id = workq.fetch_add(GRANULARITY_PUSH, std::memory_order_relaxed); id < length; id = workq.fetch_add(GRANULARITY_PUSH, std::memory_order_relaxed)) {
-    int stop = (id + GRANULARITY_PUSH) > length ? length : (id + GRANULARITY_PUSH);
-    for (int d = start + id; d < start + stop; d++) {
-      register int temp_new = 0.0;
-      register int temp_old = old_rank[d];
-      register int error = 0.0;
-      register int first = in_indices[d];
-      register int last = in_indices[d+1];
-      register int od = out_degree[d];
-      int s = first;
-      for(; s < last; s++) {
-        register int idx = in_neighbors[s];
-        register int tmp = contrib[idx];
-        temp_new = temp_new + tmp;
+      int stop = (id + GRANULARITY_PUSH) > length ? length : (id + GRANULARITY_PUSH);
+      for (int s = start + id; s < start + stop; s++) {
+          // read your contribution
+          register int s_contrib = contrib[s];
+          register int first = out_indices[s];
+          register int last  = out_indices[s+1];
+          int d = first;
+          for (; d < last; d++) {
+              // add contribution to d            
+              register int idx = out_neighbors[d];
+              bsg_amoadd(&new_rank[idx], s_contrib);
+          }        
       }
-
-      temp_new = beta_score + damp * temp_new;
-      error = fabs(temp_new - temp_old);
-      old_rank[d] = temp_new;
-      contrib_new[d] = temp_new / od;
-    }
+  }
+  bsg_barrier_hw_tile_group_sync();
+  ///////////////////
+  // update vertex //
+  ///////////////////
+  for (int id = __bsg_id*STRIDE; id < V; id += bsg_tiles_X*bsg_tiles_Y*STRIDE) {
+      // 1. set old rank and new rank
+      int tmp = bsg_amoadd(&new_rank[id], beta_score);
+      old_rank[id] = 0;
+      // 2. compute new contribution
+      new_contrib[id] = tmp / out_degree[id];
+      // host will swap old_rank and new_rank
   }
 
   bsg_cuda_print_stat_kernel_end();
@@ -75,76 +82,22 @@ int __attribute__ ((noinline)) pagerank_push(int bsg_attr_remote * bsg_attr_noal
 
 
 extern "C"
-int __attribute__ ((noinline)) pagerank_push_u8(int bsg_attr_remote * bsg_attr_noalias in_indices,
-                                                int bsg_attr_remote * bsg_attr_noalias in_neighbors,
+int __attribute__ ((noinline)) pagerank_push_u8(int bsg_attr_remote * bsg_attr_noalias out_indices,
+                                                int bsg_attr_remote * bsg_attr_noalias out_neighbors,
                                                 int bsg_attr_remote * bsg_attr_noalias out_degree,
                                                 int bsg_attr_remote * bsg_attr_noalias old_rank,
                                                 int bsg_attr_remote * bsg_attr_noalias new_rank,
                                                 int bsg_attr_remote * bsg_attr_noalias contrib,
                                                 int bsg_attr_remote * bsg_attr_noalias contrib_new,
                                                 int V) {
-  bsg_barrier_hw_tile_group_init();
-  bsg_cuda_print_stat_kernel_start();
-
-  int start = 0;
-  int end = V;
-  int length = end - start;
-  for(int id = workq.fetch_add(GRANULARITY_PUSH, std::memory_order_relaxed); id < length; id = workq.fetch_add(GRANULARITY_PUSH, std::memory_order_relaxed)) {
-    int stop = (id + GRANULARITY_PUSH) > length ? length : (id + GRANULARITY_PUSH);
-    for (int d = start + id; d < start + stop; d++) {
-      register int temp_new = 0.0f;
-      register int temp_old = old_rank[d];
-      register int error = 0.0f;
-      register int first = in_indices[d];
-      register int last = in_indices[d+1];
-      register int od = out_degree[d];
-      int s = first;
-      for(;s + 8 < last; s += 8) {
-              //              bsg_unroll(8) for(int si = 0; si < 8; ++si){
-              register int idx0 asm ("s8");
-              register int idx1 asm ("s9");
-              register int idx2 asm ("s10");
-              register int idx3 asm ("s11");
-              register int idx4 asm ("t3");
-              register int idx5 asm ("t4");
-              register int idx6 asm ("t5");
-              register int idx7 asm ("t6");
-              idx0 = in_neighbors[s + 0];
-              idx1 = in_neighbors[s + 1];
-              idx2 = in_neighbors[s + 2];
-              idx3 = in_neighbors[s + 3];
-              idx4 = in_neighbors[s + 4];
-              idx5 = in_neighbors[s + 5];
-              idx6 = in_neighbors[s + 6];
-              idx7 = in_neighbors[s + 7];
-              asm volatile ("" ::: "memory");
-              temp_new += contrib[idx0];
-              temp_new += contrib[idx1];
-              temp_new += contrib[idx2];
-              temp_new += contrib[idx3];
-              temp_new += contrib[idx4];
-              temp_new += contrib[idx5];
-              temp_new += contrib[idx6];
-              temp_new += contrib[idx7];
-              //temp_new += contrib[idx];
-                      //              }
-      }
-
-      for(; s < last; ++s){
-              register int idx = in_neighbors[s];
-              register int tmp = contrib[idx];
-              temp_new = temp_new + tmp;
-      }
-
-      temp_new = beta_score + damp * temp_new;
-      error = fabs(temp_new - temp_old);
-      old_rank[d] = temp_new;
-      contrib_new[d] = temp_new / od;
-    }
-  }
-
-  bsg_cuda_print_stat_kernel_end();
-  bsg_fence();
-  bsg_barrier_hw_tile_group_sync();
-  return 0;
+    
+    return pagerank_push(
+        out_indices,
+        out_neighbors,
+        out_degree,
+        old_rank,
+        new_rank,
+        contrib,
+        contrib_new
+        );
 }
