4c00bc8ab08c7654a517937926aa33806168d57c
Tommy Jung <tommy.dc.jung@gmail.com>
2023-01-31 10:34:27 -0800

    revert

--- apps/memcpy/tests.mk
+++ apps/memcpy/tests.mk
@@ -13,10 +13,10 @@
 #TESTS += $(call test-name,16,8,524288,no)
 #TESTS += $(call test-name,16,16,524288,no)
 
-MSIZE = 524288
+MSIZE = 522488
 TESTS += $(call test-name,4,2,$(MSIZE),no)
 TESTS += $(call test-name,4,4,$(MSIZE),no)
 TESTS += $(call test-name,8,4,$(MSIZE),no)
 TESTS += $(call test-name,8,8,$(MSIZE),no)
 TESTS += $(call test-name,16,8,$(MSIZE),no)
-#TESTS += $(call test-name,16,16,$(MSIZE),no)
+TESTS += $(call test-name,16,16,$(MSIZE),no)
