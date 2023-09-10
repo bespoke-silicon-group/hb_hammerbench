7563af62e7e280fbaa392bc713ab45cc259f835b
tj <tommy.dc.jung@gmail.com>
2023-09-10 13:53:05 -0700

    bfs frontier cost

--- apps/bfs-edge-parallel-multipod/inputs/frontier_cost.py
+++ apps/bfs-edge-parallel-multipod/inputs/frontier_cost.py
@@ -1,6 +1,7 @@
 import sys
 
 graph_name = sys.argv[1]
+V = int(sys.argv[2])
 
 # load distance data;
 distances = []
@@ -11,8 +12,19 @@ with open("{}.distance.txt".format(graph_name), "r") as f0:
 
 
 max_distance = max(distances)
+dist_count = [0] * (max_distance+1)
+for d in distances:
+  dist_count[d] += 1
+
+total_overhead = 0
 for i in range(max_distance+1):
-  count = distances.count(i)
+  count = dist_count[i]
   # for 8x8, 16x8 pods
-  overhead = (count*2)
+  average_hop = 50
+  overhead = (count*2*average_hop/(16*32)) + (V/32/64/0.8/8)
+  total_overhead += overhead
   print("d={}, frontier_size={}, overhead={}".format(i, count, overhead))
+
+print("total_overhead = {}".format(total_overhead))
+
+
