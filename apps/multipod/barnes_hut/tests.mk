587f424f3c0eabff4b8a14830f2071f23d5e44fe
Tommy Jung <46542701+tommydcjung@users.noreply.github.com>
2023-10-13 08:11:51 -0700

    adding slow pod runs (#42)

--- apps/multipod/barnes_hut/tests.mk
+++ apps/multipod/barnes_hut/tests.mk
@@ -7,3 +7,8 @@ POD_RANGE = $(shell seq -s " " 0 8 63)
 #$(foreach x, $(POD_RANGE), $(eval TESTS += $(call test-name,32768,$(x))))
 # 64k
 #$(foreach x, $(POD_RANGE), $(eval TESTS += $(call test-name,65536,$(x))))
+
+# known-slowest pods;
+TESTS += $(call test-name,16384,0)
+TESTS += $(call test-name,32768,0)
+TESTS += $(call test-name,65536,56)
--- apps/multipod/spgemm/tests.mk
+++ apps/multipod/spgemm/tests.mk
@@ -9,6 +9,13 @@ POD_RANGE = $(shell seq -s " " 0 63)
 # offshore
 #$(foreach x, $(POD_RANGE), $(eval TESTS += $(call test-name,offshore,$(x))))
 # road central
-$(foreach x, $(POD_RANGE), $(eval TESTS += $(call test-name,road-central,$(x))))
+#$(foreach x, $(POD_RANGE), $(eval TESTS += $(call test-name,road-central,$(x))))
 # roadNet CA
 #$(foreach x, $(POD_RANGE), $(eval TESTS += $(call test-name,roadNet-CA,$(x))))
+
+
+# known-slow pods;
+TESTS += $(call test-name,wiki-Vote,0)
+TESTS += $(call test-name,offshore,51)
+TESTS += $(call test-name,roadNet-CA,18)
+TESTS += $(call test-name,road-central,16)
