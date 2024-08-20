e191c9ac3938bd7f862abcd4c1b02a3f82d01a03
tj <tommy.dc.jung@gmail.com>
2024-08-20 15:49:51 -0700

    vector add

---
+++ apps/vector_add/kernel.cpp
@@ -0,0 +1,47 @@
+#include <bsg_manycore.h>
+#include <bsg_cuda_lite_barrier.h>
+
+#ifdef WARM_CACHE
+__attribute__((noinline))
+static void warmup(float *A, float *B, float *C, int N)
+{
+  for (int i = __bsg_id*CACHE_LINE_WORDS; i < N; i += bsg_tiles_X*bsg_tiles_Y*CACHE_LINE_WORDS) {
+      asm volatile ("lw x0, %[p]" :: [p] "m" (A[i]));
+      asm volatile ("lw x0, %[p]" :: [p] "m" (B[i]));
+      asm volatile ("sw x0, %[p]" :: [p] "m" (C[i]));
+  }
+  bsg_fence();
+}
+#endif
+
+
+// Vector-Add: C = A + B
+// N = vector size
+extern "C" __attribute__ ((noinline))
+int
+kernel_vector_add(float * A, float * B, float *C, int N) {
+
+  bsg_barrier_hw_tile_group_init();
+#ifdef WARM_CACHE
+  warmup(A, B, C, N);
+#endif
+  bsg_barrier_hw_tile_group_sync();
+  bsg_cuda_print_stat_kernel_start();
+
+  // Each tile does a portion of vector_add
+  int len = N / (bsg_tiles_X*bsg_tiles_Y);
+  float *myA = &A[__bsg_id*len];
+  float *myB = &B[__bsg_id*len];
+  float *myC = &C[__bsg_id*len];
+
+  for (int i = 0; i < len; i++) {
+    myC[i] = myA[i] + myB[i];
+  }
+
+  bsg_fence();
+  bsg_cuda_print_stat_kernel_end();
+  bsg_fence();
+  bsg_barrier_hw_tile_group_sync();
+
+  return 0;
+}
