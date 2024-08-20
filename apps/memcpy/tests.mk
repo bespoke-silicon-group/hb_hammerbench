c56a87e9bba8f25914b06a76d9f6c62a9edef3b3
tj <tommy.dc.jung@gmail.com>
2024-08-20 15:42:21 -0700

    set memcpy size

--- apps/memcpy/tests.mk
+++ apps/memcpy/tests.mk
@@ -2,6 +2,6 @@
 # CHANGE ME: TESTS				        #
 # TESTS += $(call test-name,[buffer-size],[warm-cache]) #
 #########################################################
-#MSIZE = 524288
-MSIZE = 4096
+MSIZE = 524288
+#MSIZE = 4096
 TESTS += $(call test-name,16,8,$(MSIZE),no)
