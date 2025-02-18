5a6a945b49f6d2bb6ffc90910674d9b5c9ef8e13
Max Ruttenberg <mrutt@cs.washington.edu>
2025-02-18 14:00:03 -0800

    resurects appl fib

--- lib/appl/appl-hw-barrier.hpp
+++ lib/appl/appl-hw-barrier.hpp
@@ -6,18 +6,26 @@
 #define APPL_HW_BARRIER_H
 
 #include "bsg_manycore.h"
+// #ifdef ENABLE_HW_BARRIER
+// #undef ENABLE_HW_BARRIER
+// #endif
 #include "bsg_cuda_lite_barrier.h"
 #include "bsg_barrier_amoadd.h"
 
 namespace appl {
 
+__attribute__((weak, section((".dmem"))))
+int sense = 1;
+
 inline void config_hw_barrier() {
-  bsg_barrier_hw_tile_group_init();
+  //bsg_barrier_tile_group_init();
 }
 
 inline void sync() {
   bsg_fence();
-  bsg_barrier_hw_tile_group_sync();
+  //bsg_barrier_tile_group_sync();
+  // use the amoadd barrier explicitly
+  bsg_barrier_amoadd(&__cuda_barrier_cfg[0], &sense);  
   bsg_fence();
 }
 
