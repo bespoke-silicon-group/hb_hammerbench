8fe496ec528623b986ae964c8f1e9a42f47e8f10
Max Ruttenberg <mrutt@cs.washington.edu>
2025-02-20 13:29:53 -0800

    cello_hello_world tests invoke with test_eq infrastructure

--- apps/cello_hello_world/kernel.cpp
+++ apps/cello_hello_world/kernel.cpp
@@ -1,16 +1,25 @@
 #include <cello/cello.hpp>
 #include <bsg_manycore.h>
+#include <atomic>
+#include <util/test_eq.hpp>
 
 extern "C" int setup(unsigned pod_x, unsigned pod_y)
 {
     return 0;
 }
 
+__attribute__((section(".dram")))
+std::atomic<int> sched_ctr, invoke_ctr, lvalue_ctr;
+__attribute__((section(".dram")))
+std::atomic<int> sched_mask, invoke_mask, lvalue_mask;
+
 void recurse_scheduler(int depth)
 {
     using namespace cello;
     bsg_print_int(depth);
     if (depth == 0) {
+        sched_ctr++;
+        sched_mask |= (1 << __bsg_id);
         return;
     } else {
         joiner j;
@@ -27,6 +36,8 @@ void recurse_invoke(int depth)
     using namespace cello;
     bsg_print_int(depth);
     if (depth == 0) {
+        invoke_ctr++;
+        invoke_mask |= (1 << __bsg_id);
         return;
     } else {
         parallel_invoke([=]() { recurse_invoke(depth - 1); },
@@ -42,21 +53,41 @@ struct recurser
         using namespace cello;
         bsg_print_int(depth_);
         if (depth_ == 0) {
+            lvalue_ctr++;
+            lvalue_mask |= (1 << __bsg_id);
             return;
         } else {
-            recurser l(depth_ - 1), r(depth_ - 1);
-            recurser *lp = bsg_tile_group_remote_pointer<recurser>(__bsg_x, __bsg_y, &l);
-            recurser *rp = bsg_tile_group_remote_pointer<recurser>(__bsg_x, __bsg_y, &r);
-            parallel_invoke(*lp, *rp);
+            recurser c(depth_ - 1);
+            parallel_invoke(c, c);
         }
     }
     FIELD(int, depth);
 };
+
+void recurse_invoke_lvalue(int depth)
+{
+    recurser r(depth);
+    r();
+}
 int cello_main(int argc, char *argv[])
 {
     using namespace cello;
-    //recurse_invoke(6);
-    recurser r(6);
-    r();
-    return 0;
+    sched_ctr = 0;
+    invoke_ctr = 0;
+    lvalue_ctr = 0;
+    sched_mask = 0;
+    invoke_mask = 0;
+    lvalue_mask = 0;
+    recurse_scheduler(3);
+    recurse_invoke(3);
+    recurse_invoke_lvalue(3);
+    // check that the right number of leafs were reached
+    TEST_EQ(INT, sched_ctr  ,8);
+    TEST_EQ(INT, invoke_ctr ,8);
+    TEST_EQ(INT, lvalue_ctr ,8);
+    // check that more than this thread participated
+    TEST_NEQ(INT, sched_mask  ,(1 << __bsg_id));
+    TEST_NEQ(INT, invoke_mask ,(1 << __bsg_id));
+    TEST_NEQ(INT, lvalue_mask ,(1 << __bsg_id));
+    return
 }
