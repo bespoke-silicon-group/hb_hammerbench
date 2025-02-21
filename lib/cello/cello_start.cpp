a36f5e7054e19d5742d4f2a5042b8d1cf11a77d0
Max Ruttenberg <mrutt@cs.washington.edu>
2025-02-21 13:10:18 -0800

    adds print stats in cello_start

--- lib/cello/cello_start.cpp
+++ lib/cello/cello_start.cpp
@@ -22,13 +22,15 @@ int cello_start(cello::config *config)
     
     scheduler_initialize(config);    
     bsg_barrier_tile_group_sync();
-    
+    bsg_cuda_print_stat_kernel_start();
     if (__bsg_id == 0) {
         cello_main(0, nullptr);
         main_complete = 1;
     } else {
         scheduler_loop([](){ return main_complete == 1; });
     }
+    bsg_barrier_tile_group_sync();
+    bsg_cuda_print_stat_kernel_end();
     bsg_barrier_tile_group_sync();
     return 0;
 }
