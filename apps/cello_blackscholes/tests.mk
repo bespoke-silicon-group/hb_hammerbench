94f820a46c7c4d4dcbfbcf216cdb333bfa2c6b3b
Max Ruttenberg <mrutt@cs.washington.edu>
2025-03-18 12:00:41 -0700

    blackscholes larger input

--- apps/cello_blackscholes/tests.mk
+++ apps/cello_blackscholes/tests.mk
@@ -2,6 +2,6 @@
 # CHANGE ME: TESTS #
 ####################
 # TESTS += $(call test-name,[num_options])
-TESTS += $(call test-name,32)
-
+#TESTS += $(call test-name,32)
+TESTS += $(call test-name,32768)
 
