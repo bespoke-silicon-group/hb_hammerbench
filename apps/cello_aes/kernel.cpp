0f70095e21dfb5ed32dac0d604f0b3aaed7104e5
mrutt92 <mrutt@cs.washington.edu>
2025-03-21 12:02:23 -0700

    Tpr/aes (#62)
    * cello aes ported and running on single pod
    
    * working on multipod, removes suboptimal data movement

---
+++ apps/cello_aes/kernel.cpp
@@ -0,0 +1,17 @@
+#include <cello/cello.hpp>
+#include <util/statics.hpp>
+#include "common.hpp"
+#include "aes_kernel.hpp"
+
+DRAM(ctx_vector_t)     ctx_v;
+DRAM(message_vector_t) message_v;
+
+int cello_main(int argc, char *argv[])
+{
+    message_v.foreach([](int i, message_t &message){
+        bsg_print_int(i);
+        AES_ctx &ctx = ctx_v.local(i);
+        AES_CBC_encrypt_buffer(&ctx, message.data(), message.size());
+    });
+    return 0;
+}
