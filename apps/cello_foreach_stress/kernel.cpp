e1dce940a3bd0993a4d869ec3848ff6e9c89c5f4
Max Ruttenberg <mrutt@cs.washington.edu>
2025-03-31 16:10:45 -0700

    improve foreach test

--- apps/cello_foreach_stress/kernel.cpp
+++ apps/cello_foreach_stress/kernel.cpp
@@ -1,11 +1,15 @@
 #include <cello/cello.hpp>
 #include <util/statics.hpp>
 DRAM(int) niter = NITERS;
+DRAM(std::atomic<int>) counter;
+DRAM(int) data[NITERS];
+
 int cello_main(int argc, char *argv[])
 {
     int n = niter;
     cello::foreach(0, n, [](int i){
-        bsg_print_int(i);
+        data[i] = i;
+        counter++;
     });
     return 0;
 }
