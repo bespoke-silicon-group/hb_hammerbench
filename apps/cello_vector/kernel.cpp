f4a1cf38ce3994dfc95f04d91bbd12d69f80754b
Max Ruttenberg <mrutt@cs.washington.edu>
2025-04-01 13:40:04 -0700

    slims down cello_vector

--- apps/cello_vector/kernel.cpp
+++ apps/cello_vector/kernel.cpp
@@ -5,6 +5,9 @@
 
 using namespace cello;
 
+//#define COUNT
+//#define TRACE
+
 #define pr(fmt, ...)                            \
     do { bsg_printf("pod[x=%2d,y=%2d], tile[x=%2d,y=%2d]: " fmt, my::pod_x(), my::pod_y(), my::tile_x(), my::tile_y(), ##__VA_ARGS__); } while (0)
 
@@ -14,26 +17,34 @@ DRAM(std::atomic<int>) counter;
 
 int cello_main(int argc, char *argv[])
 {
+#ifdef TRACE
     on_every_pod([](){
         pr("vec_int.data() = %p\n", vec_int.data());
         pr("vec_int.size() = %d\n", vec_int.size());
     });
-
+#endif
     vec_int.foreach([](int i, int &v) {
         if (i != v)
+#ifdef TRACE
             pr("FAIL: vec_int[%2d] = %2d\n", i, v);
+#else
+            bsg_print_int(-i);
+#endif
         v = 2*v;
+#ifdef COUNT
         bsg_global_pointer::pod_address paddr;
         paddr.set_pod_x(0).set_pod_y(0);
         bsg_global_pointer::pod_address_guard grd(paddr);
         counter++;
+#endif
     });
 
+#ifdef COUNT
     if (counter != vec_int.size()) {
         pr("FAIL: counter = %d, size = %d\n",
            counter.load(),
            vec_int.size());
     }
-
+#endif
     return 0;
 }
