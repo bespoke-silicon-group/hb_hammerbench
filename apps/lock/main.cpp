a938f49a4ac6847937a99140a36351a90a91d48e
Max Ruttenberg <mrutt@cs.washington.edu>
2025-04-02 11:51:27 -0700

    one pod at a time

--- apps/lock/main.cpp
+++ apps/lock/main.cpp
@@ -10,10 +10,13 @@ int program_main(int argc, char *argv[])
     hb_mc_device_foreach_pod_id(&mc, pod_id)
     {
         BSG_CUDA_CALL(hb_mc_device_pod_program_init(&mc, pod_id, argv[1]));
-        BSG_CUDA_CALL(hb_mc_device_pod_kernel_enqueue(&mc, pod_id, {1,1}, {bsg_tiles_X,bsg_tiles_Y}, "kernel", 0, nullptr));
     }
 
-    BSG_CUDA_CALL(hb_mc_device_pods_kernels_execute(&mc));
+    hb_mc_device_foreach_pod_id(&mc, pod_id)
+    {
+        BSG_CUDA_CALL(hb_mc_device_pod_kernel_enqueue(&mc, pod_id, {1,1}, {bsg_tiles_X,bsg_tiles_Y}, "kernel", 0, nullptr));
+        BSG_CUDA_CALL(hb_mc_device_pods_kernels_execute(&mc));
+    }
 
     hb_mc_device_foreach_pod_id(&mc, pod_id)
     {
