3ad6f4c85da427772b7aacae50ab8023b7cd885c
Max Ruttenberg <mrutt@cs.washington.edu>
2025-04-16 15:40:19 -0700

    updates working inputs of spgemm

--- apps/cello_spgemm/tests.mk
+++ apps/cello_spgemm/tests.mk
@@ -3,6 +3,12 @@
 ####################
 # TESTS += $(call test-name,[mtx-a],[mtx-b])
 TESTS += $(call test-name,u7k1,g7k1)
+TESTS += $(call test-name,email-Enron,email-Enron)
+TESTS += $(call test-name,u15k16,g15k16)
+TESTS += $(call test-name,g14k16,u14k16)
+TESTS += $(call test-name,roadNet-CA,roadNet-CA)
+TESTS += $(call test-name,u16k4,g16k4)
+TESTS += $(call test-name,web-NotreDame,web-NotreDame)
 #TESTS += $(call test-name,u7k1,wiki-Vote)
 #TESTS += $(call test-name,u16k16,g16k16)
 
