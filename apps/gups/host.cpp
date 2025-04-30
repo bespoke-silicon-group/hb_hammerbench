af28ba025349247cee7b8d846a965f75bd0f5480
Max Ruttenberg <mrutt@cs.washington.edu>
2025-04-30 16:05:06 -0700

    allows more than 512 updates per core

--- apps/gups/host.cpp
+++ apps/gups/host.cpp
@@ -2,6 +2,8 @@
 #include <algorithm>
 #include <random>
 
+#define SEEDS_PER_THREAD (UPDATES_PER_THREAD > 512 ? 512 : UPDATES_PER_THREAD)
+
 using namespace bsg_global_pointer;
 
 class program : public standard::program {
@@ -9,7 +11,7 @@ public:
     int init(int argc, char *argv[]) override {
         standard::program::init(argc, argv);        
         table_ptr = find<hb_mc_eva_t>("g_table");
-        updates_ptr = find<hb_mc_eva_t>("g_updates");
+        seeds_ptr = find<hb_mc_eva_t>("g_seeds");
         return 0;
     }
 
@@ -19,37 +21,37 @@ public:
         hb_mc_device_foreach_pod_id(&mc, pod_id) {
             auto pod = pod_id_to_coord(pod_id);
             table_ptr.set_pod_x(pod.x).set_pod_y(pod.y);
-            updates_ptr.set_pod_x(pod.x).set_pod_y(pod.y);
+            seeds_ptr.set_pod_x(pod.x).set_pod_y(pod.y);
 
             hb_mc_eva_t eva;
             BSG_CUDA_CALL(hb_mc_device_pod_malloc
                           (&mc, pod_id, TABLE_SIZE * sizeof(uint32_t), &eva));
             *table_ptr = eva;
             BSG_CUDA_CALL(hb_mc_device_pod_malloc
-                          (&mc, pod_id, updates() * sizeof(uint32_t), &eva));
-            *updates_ptr = eva;
+                          (&mc, pod_id, seeds() * sizeof(uint32_t), &eva));
+            *seeds_ptr = eva;
         }
 
-        h_updates.resize(mc.num_pods);
+        h_seeds.resize(mc.num_pods);
         hb_mc_device_foreach_pod_id(&mc, pod_id) {
-            h_updates[pod_id].resize(updates());
-            for (size_t i = 0; i < h_updates[pod_id].size(); i++) {
-                h_updates[pod_id][i] = rand() % table_size();
+            h_seeds[pod_id].resize(seeds());
+            for (size_t i = 0; i < h_seeds[pod_id].size(); i++) {
+                h_seeds[pod_id][i] = rand() % table_size();
             }
             jobs_in[pod_id].push_back({
-                    *updates_ptr
-                    , h_updates[pod_id].data()
-                    , h_updates[pod_id].size()*sizeof(uint32_t)
+                    *seeds_ptr
+                    , h_seeds[pod_id].data()
+                    , h_seeds[pod_id].size()*sizeof(uint32_t)
                 });
         }
         return 0;
     }
 
-    int updates() const { return bsg_tiles_X*bsg_tiles_Y*UPDATES_PER_THREAD; }
+    int seeds() const { return bsg_tiles_X*bsg_tiles_Y*SEEDS_PER_THREAD; }
     int table_size() const { return TABLE_SIZE; }
     pointer<hb_mc_eva_t> table_ptr;
-    pointer<hb_mc_eva_t> updates_ptr;
-    std::vector<std::vector<int>> h_updates;
+    pointer<hb_mc_eva_t> seeds_ptr;
+    std::vector<std::vector<int>> h_seeds;
 };
 
 standard::program *make_program()
--- apps/gups/kernel.cpp
+++ apps/gups/kernel.cpp
@@ -3,38 +3,40 @@
 #include <util/statics.hpp>
 #include <bsg_manycore.h>
 
+#define SEEDS_PER_THREAD (UPDATES_PER_THREAD > 512 ? 512 : UPDATES_PER_THREAD)
+
 DRAM(unsigned *) g_table;
-DRAM(unsigned *) g_updates;
-DMEM(unsigned) l_updates [UPDATES_PER_THREAD];
+DRAM(unsigned *) g_seeds;
+DMEM(unsigned) l_seeds [SEEDS_PER_THREAD];
 
 int setup()
 {
     bsg_barrier_tile_group_init();
     bsg_barrier_tile_group_sync();
-    unsigned *updates_p = &g_updates[UPDATES_PER_THREAD*__bsg_id];
+    unsigned *seeds_p = &g_seeds[SEEDS_PER_THREAD*__bsg_id];
     int i = 0;
-    for (; i+8 <= UPDATES_PER_THREAD; i += 8) {
+    for (; i+8 <= SEEDS_PER_THREAD; i += 8) {
         register unsigned k[8];
-        k[0] = updates_p[i+0];
-        k[1] = updates_p[i+1];
-        k[2] = updates_p[i+2];
-        k[3] = updates_p[i+3];
-        k[4] = updates_p[i+4];
-        k[5] = updates_p[i+5];
-        k[6] = updates_p[i+6];
-        k[7] = updates_p[i+7];
+        k[0] = seeds_p[i+0];
+        k[1] = seeds_p[i+1];
+        k[2] = seeds_p[i+2];
+        k[3] = seeds_p[i+3];
+        k[4] = seeds_p[i+4];
+        k[5] = seeds_p[i+5];
+        k[6] = seeds_p[i+6];
+        k[7] = seeds_p[i+7];
         asm volatile ("" ::: "memory");
-        l_updates[i+0] = k[0];
-        l_updates[i+1] = k[1];
-        l_updates[i+2] = k[2];
-        l_updates[i+3] = k[3];
-        l_updates[i+4] = k[4];
-        l_updates[i+5] = k[5];
-        l_updates[i+6] = k[6];
-        l_updates[i+7] = k[7];
+        l_seeds[i+0] = k[0];
+        l_seeds[i+1] = k[1];
+        l_seeds[i+2] = k[2];
+        l_seeds[i+3] = k[3];
+        l_seeds[i+4] = k[4];
+        l_seeds[i+5] = k[5];
+        l_seeds[i+6] = k[6];
+        l_seeds[i+7] = k[7];
     }
-    for (int i = 0; i < UPDATES_PER_THREAD; i++) {
-        l_updates[i] = updates_p[i];
+    for (int i = 0; i < SEEDS_PER_THREAD; i++) {
+        l_seeds[i] = seeds_p[i];
     }
     bsg_barrier_tile_group_sync();
     return 0;
@@ -46,97 +48,171 @@ int kernel()
     bsg_barrier_tile_group_sync();
     bsg_cuda_print_stat_kernel_start();
     unsigned *l_table = g_table;
-    int i = 0;
+    register unsigned a = 1664525, c = 1013904223;
 #define UNROLL_4
+    for (int j; j < UPDATES_PER_THREAD; j += SEEDS_PER_THREAD) {
+        int i = 0;
 #ifdef UNROLL_8   
-    for (; i+8 <= UPDATES_PER_THREAD; i += 8) {
-        register unsigned k[8];
-        register unsigned v[8];
-        k[0] = l_updates[i+0];
-        k[1] = l_updates[i+1];
-        k[2] = l_updates[i+2];
-        k[3] = l_updates[i+3];
-        k[4] = l_updates[i+4];
-        k[5] = l_updates[i+5];
-        k[6] = l_updates[i+6];
-        k[7] = l_updates[i+7];
-        asm volatile ("" ::: "memory");
-        v[0] = l_table[k[0]];
-        v[1] = l_table[k[1]];
-        v[2] = l_table[k[2]];
-        v[3] = l_table[k[3]];
-        v[4] = l_table[k[4]];
-        v[5] = l_table[k[5]];
-        v[6] = l_table[k[6]];
-        v[7] = l_table[k[7]];
-        asm volatile("" ::: "memory");
-        v[0] ^= k[0];
-        v[1] ^= k[1];
-        v[2] ^= k[2];
-        v[3] ^= k[3];
-        v[4] ^= k[4];
-        v[5] ^= k[5];
-        v[6] ^= k[6];
-        v[7] ^= k[7];
-        asm volatile("" ::: "memory");
-        l_table[k[0]] = v[0];
-        l_table[k[1]] = v[1];
-        l_table[k[2]] = v[2];
-        l_table[k[3]] = v[3];
-        l_table[k[4]] = v[4];
-        l_table[k[5]] = v[5];
-        l_table[k[6]] = v[6];
-        l_table[k[7]] = v[7];
-        bsg_fence();        
-    }
+        for (; i+8 <= SEEDS_PER_THREAD; i += 8) {
+            register unsigned k [8];
+            register unsigned v [8];
+            register unsigned s [8];
+            s[0] = l_seeds[i+0];
+            s[1] = l_seeds[i+1];
+            s[2] = l_seeds[i+2];
+            s[3] = l_seeds[i+3];
+            s[4] = l_seeds[i+4];
+            s[5] = l_seeds[i+5];
+            s[6] = l_seeds[i+6];
+            s[7] = l_seeds[i+7];            
+            k[0] = s[0] & (TABLE_SIZE-1);
+            k[1] = s[1] & (TABLE_SIZE-1);
+            k[2] = s[2] & (TABLE_SIZE-1);
+            k[3] = s[3] & (TABLE_SIZE-1);
+            k[4] = s[4] & (TABLE_SIZE-1);
+            k[5] = s[5] & (TABLE_SIZE-1);
+            k[6] = s[6] & (TABLE_SIZE-1);
+            k[7] = s[7] & (TABLE_SIZE-1);
+            asm volatile ("" ::: "memory");
+            v[0] = l_table[k[0]];
+            v[1] = l_table[k[1]];
+            v[2] = l_table[k[2]];
+            v[3] = l_table[k[3]];
+            v[4] = l_table[k[4]];
+            v[5] = l_table[k[5]];
+            v[6] = l_table[k[6]];
+            v[7] = l_table[k[7]];
+            asm volatile("" ::: "memory");
+            s[0] = s[0] * a;
+            s[1] = s[1] * a;
+            s[2] = s[2] * a;
+            s[3] = s[3] * a;
+            s[4] = s[4] * a;
+            s[5] = s[5] * a;
+            s[6] = s[6] * a;
+            s[7] = s[7] * a;
+
+            s[0] = s[0] + c;
+            s[1] = s[1] + c;
+            s[2] = s[2] + c;
+            s[3] = s[3] + c;
+            s[4] = s[4] + c;
+            s[5] = s[5] + c;
+            s[6] = s[6] + c;
+            s[7] = s[7] + c;
+
+            v[0] ^= k[0];
+            v[1] ^= k[1];
+            v[2] ^= k[2];
+            v[3] ^= k[3];            
+            v[4] ^= k[4];
+            v[5] ^= k[5];
+            v[6] ^= k[6];
+            v[7] ^= k[7];            
+            asm volatile("" ::: "memory");
+            l_table[k[0]] = v[0];
+            l_table[k[1]] = v[1];
+            l_table[k[2]] = v[2];
+            l_table[k[3]] = v[3];
+            l_table[k[4]] = v[4];
+            l_table[k[5]] = v[5];
+            l_table[k[6]] = v[6];
+            l_table[k[7]] = v[7];
+            asm volatile("" ::: "memory");        
+            l_seeds[i+0] = s[0];
+            l_seeds[i+1] = s[1];
+            l_seeds[i+2] = s[2];
+            l_seeds[i+3] = s[3];
+            l_seeds[i+4] = s[4];
+            l_seeds[i+5] = s[5];
+            l_seeds[i+6] = s[6];
+            l_seeds[i+7] = s[7];
+            bsg_fence();        
+        }
 #elif defined(UNROLL_4)
-    for (; i+4 <= UPDATES_PER_THREAD; i += 4) {
-        register unsigned k[4];
-        register unsigned v[4];
-        k[0] = l_updates[i+0];
-        k[1] = l_updates[i+1];
-        k[2] = l_updates[i+2];
-        k[3] = l_updates[i+3];
-        asm volatile ("" ::: "memory");
-        v[0] = l_table[k[0]];
-        v[1] = l_table[k[1]];
-        v[2] = l_table[k[2]];
-        v[3] = l_table[k[3]];
-        asm volatile("" ::: "memory");
-        v[0] ^= k[0];
-        v[1] ^= k[1];
-        v[2] ^= k[2];
-        v[3] ^= k[3];
-        asm volatile("" ::: "memory");
-        l_table[k[0]] = v[0];
-        l_table[k[1]] = v[1];
-        l_table[k[2]] = v[2];
-        l_table[k[3]] = v[3];
-        bsg_fence();        
-    }    
+        for (; i+4 <= SEEDS_PER_THREAD; i += 4) {
+            register unsigned k [4];
+            register unsigned v [4];
+            register unsigned s [4];
+            s[0] = l_seeds[i+0];
+            s[1] = l_seeds[i+1];
+            s[2] = l_seeds[i+2];
+            s[3] = l_seeds[i+3];
+            k[0] = s[0] & (TABLE_SIZE-1);
+            k[1] = s[1] & (TABLE_SIZE-1);
+            k[2] = s[2] & (TABLE_SIZE-1);
+            k[3] = s[3] & (TABLE_SIZE-1);
+            asm volatile ("" ::: "memory");
+            v[0] = l_table[k[0]];
+            v[1] = l_table[k[1]];
+            v[2] = l_table[k[2]];
+            v[3] = l_table[k[3]];
+            asm volatile("" ::: "memory");
+            s[0] = s[0] * a;
+            s[1] = s[1] * a;
+            s[2] = s[2] * a;
+            s[3] = s[3] * a;
+
+            s[0] = s[0] + c;
+            s[1] = s[1] + c;
+            s[2] = s[2] + c;
+            s[3] = s[3] + c;
+
+            v[0] ^= k[0];
+            v[1] ^= k[1];
+            v[2] ^= k[2];
+            v[3] ^= k[3];            
+            asm volatile("" ::: "memory");
+            l_table[k[0]] = v[0];
+            l_table[k[1]] = v[1];
+            l_table[k[2]] = v[2];
+            l_table[k[3]] = v[3];
+            asm volatile("" ::: "memory");        
+            l_seeds[i+0] = s[0];
+            l_seeds[i+1] = s[1];
+            l_seeds[i+2] = s[2];
+            l_seeds[i+3] = s[3];
+            bsg_fence();        
+        }
 #elif defined(UNROLL_2)
-    for (; i+2 <= UPDATES_PER_THREAD; i += 2) {
-        register unsigned k[2];
-        register unsigned v[2];
-        k[0] = l_updates[i+0];
-        k[1] = l_updates[i+1];
-        asm volatile ("" ::: "memory");
-        v[0] = l_table[k[0]];
-        v[1] = l_table[k[1]];
-        asm volatile("" ::: "memory");
-        v[0] ^= k[0];
-        v[1] ^= k[1];
-        asm volatile("" ::: "memory");
-        l_table[k[0]] = v[0];
-        l_table[k[1]] = v[1];
-        bsg_fence();        
-    }     
+        for (; i+2 <= SEEDS_PER_THREAD; i += 2) {
+            register unsigned k [2];
+            register unsigned v [2];
+            register unsigned s [2];
+            s[0] = l_seeds[i+0];
+            s[1] = l_seeds[i+1];
+            k[0] = s[0] & (TABLE_SIZE-1);
+            k[1] = s[1] & (TABLE_SIZE-1);
+            asm volatile ("" ::: "memory");
+            v[0] = l_table[k[0]];
+            v[1] = l_table[k[1]];
+            asm volatile("" ::: "memory");
+            s[0] = s[0] * a;
+            s[1] = s[1] * a;
+
+            s[0] = s[0] + c;
+            s[1] = s[1] + c;
+
+            v[0] ^= k[0];
+            v[1] ^= k[1];
+            asm volatile("" ::: "memory");
+            l_table[k[0]] = v[0];
+            l_table[k[1]] = v[1];
+            asm volatile("" ::: "memory");        
+            l_seeds[i+0] = s[0];
+            l_seeds[i+1] = s[1];
+            bsg_fence();        
+        }
 #endif
-    for (; i < UPDATES_PER_THREAD; i++) {
-        unsigned k = l_updates[i];
-        unsigned v = l_table[k];
-        v = v ^ k;
-        l_table[k] = v;
+        for (; i < SEEDS_PER_THREAD; i++) {
+            unsigned s = l_seeds[i];
+            unsigned k = s & (TABLE_SIZE-1);
+            unsigned v = l_table[k];
+            v = v ^ k;
+            l_table[k] = v;
+            s = s * a + c;
+            bsg_fence();
+        }
     }
     bsg_barrier_tile_group_sync();
     bsg_cuda_print_stat_kernel_end();
