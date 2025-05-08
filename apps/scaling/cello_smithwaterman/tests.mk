02af1103a48bd81c48b242cc58621e7de2c92eb2
Max Ruttenberg <mrutt@cs.washington.edu>
2025-05-08 15:39:53 -0700

    scaling testbench cello_smithwaterman

---
+++ apps/scaling/cello_smithwaterman/tests.mk
@@ -0,0 +1,7 @@
+####################
+# CHANGE ME: TESTS #
+####################
+# TESTS += $(call test-name,[num-seq],[tiles-x],[tiles-y],[pods-x],[pods-y])
+TESTS += $(call test-name,512,16,8,4,2)
+
+
