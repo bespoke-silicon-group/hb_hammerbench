2aae05629b61e435a8b21e98cb899a6f2491a276
Max Ruttenberg <mrutt@cs.washington.edu>
2025-05-08 15:39:16 -0700

    adds tests to blackscholes scaling

--- apps/scaling/cello_blackscholes/tests.mk
+++ apps/scaling/cello_blackscholes/tests.mk
@@ -6,5 +6,12 @@ TESTS += $(call test-name,$(shell echo 1024*1024|bc),1,1,1,1)
 TESTS += $(call test-name,$(shell echo 1024*1024|bc),1,1,2,1)
 TESTS += $(call test-name,$(shell echo 1024*1024|bc),1,1,2,2)
 TESTS += $(call test-name,$(shell echo 1024*1024|bc),1,1,4,2)
+TESTS += $(call test-name,$(shell echo 1024*1024|bc),2,1,4,2)
+TESTS += $(call test-name,$(shell echo 1024*1024|bc),2,2,4,2)
+TESTS += $(call test-name,$(shell echo 1024*1024|bc),4,2,4,2)
+TESTS += $(call test-name,$(shell echo 1024*1024|bc),4,4,4,2)
+TESTS += $(call test-name,$(shell echo 1024*1024|bc),8,4,4,2)
+TESTS += $(call test-name,$(shell echo 1024*1024|bc),8,8,4,2)
+TESTS += $(call test-name,$(shell echo 1024*1024|bc),16,8,4,2)
 
 
