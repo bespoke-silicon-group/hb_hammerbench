5407bdaf86d486a888191a320dc927e7587fcfd4
Max Ruttenberg <mrutt@cs.washington.edu>
2025-05-08 11:01:41 -0700

    scaling testbench for spgemm

---
+++ apps/scaling/cello_spgemm/tests.mk
@@ -0,0 +1,14 @@
+####################
+# CHANGE ME: TESTS #
+####################
+# TESTS += $(call test-name,[mtx-a],[mtx-b],[tiles-x],[tiles-y],[pods-x],[pods-y])
+# TESTS += $(call test-name,wiki-Vote,wiki-Vote,1,1,1,1)
+TESTS += $(call test-name,u7k1,u7k1,1,1,1,1)
+TESTS += $(call test-name,u7k1,u7k1,1,1,2,1)
+TESTS += $(call test-name,u7k1,u7k1,1,1,2,2)
+TESTS += $(call test-name,u7k1,u7k1,1,1,4,2)
+
+#TESTS += $(call test-name,wiki-Vote,wiki-Vote,1,1,1,1)
+#TESTS += $(call test-name,wiki-Vote,wiki-Vote,1,1,2,1)
+#TESTS += $(call test-name,wiki-Vote,wiki-Vote,1,1,2,2)
+TESTS += $(call test-name,wiki-Vote,wiki-Vote,1,1,4,2)
