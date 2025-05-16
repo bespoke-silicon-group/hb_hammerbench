1eacf6e240198b1aab4d9aa549c98ccf6f65371a
Max Ruttenberg <mrutt@cs.washington.edu>
2025-05-16 10:05:40 -0700

    move vector metadata into dmem

--- apps/cello_vector_compute/kernel.cpp
+++ apps/cello_vector_compute/kernel.cpp
@@ -3,10 +3,19 @@
 #include "common.hpp"
 
 DRAM(vec) V;
+DMEM(vec) Vl;
+
+static void setup()
+{
+    Vl = V;
+    return;
+}
+
+cello_constructor(setup);
 
 int cello_main(int argc, char *argv[])
 {
-    V.foreach([](int i, int &v){
+    Vl.foreach([](int i, int &v){
         for (int j = 0; j < COMPUTE; j++) {
             asm volatile("addi x0, x0, 1");
             asm volatile("addi x0, x0, 1");
