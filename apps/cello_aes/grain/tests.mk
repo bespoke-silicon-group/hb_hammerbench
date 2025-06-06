ff5813b7508dd3313cb3f1682f9347126fcd2227
Max Ruttenberg <mrutt@cs.washington.edu>
2025-06-06 12:11:13 -0700

    grain scaling testbenches for aes, barnes-hut, blackscholes, smithwaterman, and spgemm

---
+++ apps/cello_aes/grain/tests.mk
@@ -0,0 +1,14 @@
+
+####################
+# CHANGE ME: TESTS #
+####################
+# TESTS += $(call test-name,[num-iter],[tiles-x],[tiles-y],[pods-x],[pods-y],[grain])
+
+TESTS += $(call test-name,65536,16,8,4,2,1)
+TESTS += $(call test-name,65536,16,8,4,2,2)
+TESTS += $(call test-name,65536,16,8,4,2,3)
+TESTS += $(call test-name,65536,16,8,4,2,4)
+TESTS += $(call test-name,65536,16,8,4,2,5)
+TESTS += $(call test-name,65536,16,8,4,2,6)
+TESTS += $(call test-name,65536,16,8,4,2,7)
+TESTS += $(call test-name,65536,16,8,4,2,8)
