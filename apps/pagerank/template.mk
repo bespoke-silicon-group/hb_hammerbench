4d4e47abecf9b049bcf1bd04662bf1606c7b8d5b
Paul Gao <gaozihou@uw.edu>
2025-06-06 23:57:57 -0700

    Add graph apps

--- apps/pagerank/template.mk
+++ apps/pagerank/template.mk
@@ -6,10 +6,10 @@ include ../config.$(graph).mk
 
 # Hardware;
 HB_HAMMERBENCH_PATH:=$(shell git rev-parse --show-toplevel)
-NUMPODS?=64
-tile-x?=16
-tile-y?=8
-override BSG_MACHINE_PATH = $(REPLICANT_PATH)/machines/pod_X1Y1_ruche_X$(tile-x)Y$(tile-y)_hbm_one_pseudo_channel
+NUMPODS?=1
+tile-x?=4
+tile-y?=4
+#override BSG_MACHINE_PATH = $(REPLICANT_PATH)/machines/pod_X1Y1_ruche_X$(tile-x)Y$(tile-y)_hbm_one_pseudo_channel
 include $(HB_HAMMERBENCH_PATH)/mk/environment.mk
 
 
