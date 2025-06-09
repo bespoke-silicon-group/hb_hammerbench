58a778b6cf825bdb3675837ffec712a8315730d0
mrutt92 <mrutt@cs.washington.edu>
2025-06-09 13:24:31 -0700

    Tpr/grain scaling (#83)
    * set grain scales with an ifdef
    
    * grain scaling testbenches for aes, barnes-hut, blackscholes,
    smithwaterman, and spgemm

--- apps/cello_aes/kernel.cpp
+++ apps/cello_aes/kernel.cpp
@@ -3,12 +3,22 @@
 #include "common.hpp"
 #include "aes_kernel.hpp"
 
+#ifndef GRAIN_SCALE
+#define GRAIN_SCALE 8
+#endif
+
 DRAM(ctx_vector_t)     ctx_v;
 DRAM(message_vector_t) message_v;
 
 int cello_main(int argc, char *argv[])
 {
-    message_v.foreach([](int i, message_t &message){
+    int grain = message_v.local_size()/(cello::threads()*GRAIN_SCALE);
+    if (grain < 1)
+        grain = 1;
+
+    bsg_print_int(message_v.size());
+    bsg_print_int(grain);
+    message_v.foreach(grain, [](int i, message_t &message){
 #ifdef TRACE
         bsg_print_int(i);
 #endif
---
+++ apps/cello_smithwaterman/grain/bigblade-tests.py
@@ -0,0 +1,31 @@
+def test_str(num_seq, tiles_x, tiles_y, pods_x, pods_y, grain_scale):
+    return f'TESTS += $(call test-name,{num_seq},{tiles_x},{tiles_y},{pods_x},{pods_y},{grain_scale})'
+
+header = """
+####################
+# CHANGE ME: TESTS #
+####################
+# TESTS += $(call test-name,[num-seq],[tiles-x],[tiles-y],[pods-x],[pods-y],[grain])
+"""
+
+inputs = [32*1024]
+
+cores = [
+    (1,1,4,2),
+    (2,1,4,2),
+    (2,2,4,2),
+    (4,2,4,2),
+    (4,4,4,2),
+    (8,4,4,2),
+    (8,8,4,2),
+    (16,8,4,2)
+]
+
+grains = [1,2,3,4,5,6,7,8]
+
+print(header)
+
+for grain in grains:
+    for inp in inputs:
+        for (tiles_x,tiles_y,pods_x,pods_y) in cores:
+            print(test_str(inp,tiles_x,tiles_y,pods_x,pods_y,grain))
