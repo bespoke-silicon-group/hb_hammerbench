e6391bdcfd82727769a9fd96979233c1920d72ea
Max Ruttenberg <mrutt@cs.washington.edu>
2025-07-02 10:14:04 -0700

    cello_delegate microbenchmark

--- apps/cello_delegate/tests.mk
+++ apps/cello_delegate/tests.mk
@@ -1,7 +1,10 @@
+
 ####################
 # CHANGE ME: TESTS #
 ####################
-# TESTS += $(call test-name,[n])
-TESTS += $(call test-name,1)
+# TESTS += $(call test-name,[n],[task_size],[opt_memcpy],[opt_pod_address],[tiles-x],[tiles-y],[pods-x],[pods-y],[run])
 
-
+TESTS += $(call test-name,9000,32,yes,yes,16,8,4,2,0)
+TESTS += $(call test-name,9000,32,yes,no,16,8,4,2,0)
+TESTS += $(call test-name,9000,32,no,yes,16,8,4,2,0)
+TESTS += $(call test-name,9000,32,no,no,16,8,4,2,0)
