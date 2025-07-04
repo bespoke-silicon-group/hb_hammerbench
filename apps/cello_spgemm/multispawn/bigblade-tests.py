9b1fa5e0a7231fdc36671b3566257b3b9813584c
Max Ruttenberg <mrutt@cs.washington.edu>
2025-07-04 10:45:47 -0700

    updates tests

--- apps/cello_spgemm/multispawn/bigblade-tests.py
+++ apps/cello_spgemm/multispawn/bigblade-tests.py
@@ -1,5 +1,5 @@
-def test_str(mtx_a, mtx_b, tiles_x, tiles_y, pods_x, pods_y):
-    return f'TESTS += $(call test-name,{mtx_a},{mtx_b},{tiles_x},{tiles_y},{pods_x},{pods_y})'
+def test_str(mtx_a, mtx_b, multispawn, tiles_x, tiles_y, pods_x, pods_y, run):
+    return f'TESTS += $(call test-name,{mtx_a},{mtx_b},{tiles_x},{tiles_y},{pods_x},{pods_y},{multispawn},{run})'
 
 header = """
 ####################
@@ -8,23 +8,16 @@ header = """
 # TESTS += $(call test-name,[mtx-a],[mtx-b],[tiles-x],[tiles-y],[pods-x],[pods-y])
 """
 
-inputs = ['u11k16', 'u12k8', 'u13k8', 'wiki-Vote', 'email-Enron', 'roadNet-CA']
+inputs = ['email-Enron', 'roadNet-CA']
 
-cores = [( 1,1,1,1),
-         ( 1,1,2,1),
-         ( 1,1,2,2),
-         ( 1,1,4,2),
-         ( 2,1,4,2),
-         ( 2,2,4,2),
-         ( 4,2,4,2),
-         ( 4,4,4,2),
-         ( 8,4,4,2),
-         ( 8,8,4,2),
-         (16,8,4,2)]
+cores = [(16,8,4,2)]
+runs = 10
 
 print(header)
 
-for inp in inputs:
-    for (tiles_x,tiles_y,pods_x,pods_y) in cores:
-        print(test_str(inp,inp,tiles_x,tiles_y,pods_x,pods_y))
+for run in range(runs):
+    for multispawn in ("yes", "no"):
+        for inp in inputs:
+            for (tiles_x,tiles_y,pods_x,pods_y) in cores:
+                print(test_str(inp,inp,multispawn,tiles_x,tiles_y,pods_x,pods_y,run))
 
