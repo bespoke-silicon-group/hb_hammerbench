3324722779f16e45995079032c0701fbab787772
mrutt92 <mrutt@cs.washington.edu>
2025-07-07 10:00:20 -0700

    Tpr/fetch (#88)
    * fetch benchmark to measure acievable read bandwidth
    
    * fixes configuring used pods
    
    * setup to test 1 pod bandwidth on bigblade

---
+++ apps/fetch/kernel.cpp
@@ -0,0 +1,23 @@
+#include <bsg_manycore.h>
+#include <bsg_tile_config_vars.h>
+#include <bsg_cuda_lite_barrier.h>
+
+__attribute__((section(".dram"))) int *A;
+
+extern "C" int kernel()
+{
+    register int *p = &A[(__bsg_x + bsg_tiles_X*(__bsg_y == bsg_tiles_Y-1 ? 1 : 0)) * BSG_CACHE_LINE_WORDS];
+    bsg_barrier_tile_group_init();
+    bsg_barrier_tile_group_sync();    
+    bsg_cuda_print_stat_kernel_start();    
+    if (__bsg_y == 0 || __bsg_y == bsg_tiles_Y-1) {
+        for (int i = 0; i < N/(bsg_tiles_X*2); i++) {
+            asm volatile ("lw x0, %0" :: "m"(*p) : "memory");        
+            p += bsg_tiles_X * 2 * BSG_CACHE_LINE_WORDS;
+        }
+    }
+    bsg_fence();
+    bsg_cuda_print_stat_kernel_end();    
+    bsg_barrier_tile_group_sync();
+    return 0;
+}
---
+++ apps/fetch/tests.mk
@@ -0,0 +1,17 @@
+####################
+# CHANGE ME: TESTS #
+####################
+# TESTS += $(call test-name,[size],[n])
+#TESTS += $(call test-name,$(shell echo 1024|bc),$(shell echo 128|bc))
+#TESTS += $(call test-name,$(shell echo 1024*32|bc),$(shell echo 1024|bc))
+#TESTS += $(call test-name,$(shell echo 16*1024*32|bc),$(shell echo 16*1024|bc))
+#TESTS += $(call test-name,$(shell echo 1024*1024*32|bc),$(shell echo 1024*1024|bc))
+TESTS += $(call test-name,$(shell echo 1024*1024*1024|bc),$(shell echo  2*1024*1024|bc))
+TESTS += $(call test-name,$(shell echo 1024*1024*1024|bc),$(shell echo  4*1024*1024|bc))
+TESTS += $(call test-name,$(shell echo 1024*1024*1024|bc),$(shell echo  8*1024*1024|bc))
+TESTS += $(call test-name,$(shell echo 1024*1024*1024|bc),$(shell echo 16*1024*1024|bc))
+TESTS += $(call test-name,$(shell echo 1024*1024*1024|bc),$(shell echo 32*1024*1024|bc))
+TESTS += $(call test-name,$(shell echo 1024*1024*1024|bc),$(shell echo 64*1024*1024|bc))
+
+
+
