140c494c8c1bdafbf3355574c48e89beeba3f398
Max Ruttenberg <mrutt@cs.washington.edu>
2025-07-14 12:00:47 -0700

    smithwaterman baseline running

--- apps/smithwaterman/tests.mk
+++ apps/smithwaterman/tests.mk
@@ -1,1 +1,8 @@
-TESTS += $(call test-name,512)
+####################
+# CHANGE ME: TESTS #
+####################
+# TESTS += $(call test-name,[num-seq])
+#TESTS += $(call test-name,512)
+TESTS += $(call test-name,$(shell echo 1024*1024|bc))
+
+
