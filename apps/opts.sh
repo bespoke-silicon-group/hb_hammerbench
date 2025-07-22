e0dad0fe52a2b2fb19d06b3fc48d6eecf839abee
Max Ruttenberg <mrutt@cs.washington.edu>
2025-07-22 11:05:18 -0700

    updates the optimization testbenches

--- apps/opts.sh
+++ apps/opts.sh
@@ -1,5 +1,5 @@
-apps="cello_aes cello_barnes_hut cello_blackscholes cello_fft256_2d cello_smithwaterman"
+apps="cello_aes cello_barnes_hut cello_blackscholes cello_fft256_2d cello_smithwaterman cello_spgemm"
 for app in $apps; do
-    python3 $app/eval/bigblade-tests.py --runs=10 > $app/eval/tests.mk
-    make -B -C $app/eval HB_MC_DEVICE_ID=$MY_UNIT_ID report.csv
+    python3 $app/opts/bigblade-tests.py --runs=10 > $app/opts/tests.mk
+    make -C $app/opts HB_MC_DEVICE_ID=$MY_UNIT_ID report.csv
 done
