2cbda3c7aad11dcba7574365bf0ca20e90c95268
Max Ruttenberg <mrutt@cs.washington.edu>
2025-08-20 10:34:05 -0700

    fixes buffer limit issue in fetch

--- apps/fetch/kernel.cpp
+++ apps/fetch/kernel.cpp
@@ -4,16 +4,27 @@
 
 __attribute__((section(".dram"))) int *A;
 
+#define WORDS (SIZE/4)
+#define LINES (WORDS/BSG_CACHE_LINE_WORDS)
+
 extern "C" int kernel()
 {
-    register int *p = &A[(__bsg_x + bsg_tiles_X*(__bsg_y == bsg_tiles_Y-1 ? 1 : 0)) * BSG_CACHE_LINE_WORDS];
+    register int *p_start = &A[(__bsg_x + bsg_tiles_X*(__bsg_y == bsg_tiles_Y-1 ? 1 : 0)) * BSG_CACHE_LINE_WORDS];
+    register int *p = p_start;
     bsg_barrier_tile_group_init();
     bsg_barrier_tile_group_sync();    
     bsg_cuda_print_stat_kernel_start();    
     if (__bsg_y == 0 || __bsg_y == bsg_tiles_Y-1) {
-        for (int i = 0; i < N/(bsg_tiles_X*2); i++) {
-            asm volatile ("lw x0, %0" :: "m"(*p) : "memory");        
-            p += bsg_tiles_X * 2 * BSG_CACHE_LINE_WORDS;
+        for (int i = 0, j = 0; i < N; ) {
+            asm volatile ("lw x0, %0" :: "m"(*p) : "memory");
+            i += bsg_tiles_X*2;
+            j += bsg_tiles_X*2;
+            if (j > LINES) {
+                p = p_start;
+                j = 0;
+            } else {
+                p += bsg_tiles_X*2*BSG_CACHE_LINE_WORDS;
+            }
         }
     }
     bsg_fence();
--- apps/fetch/tests.mk
+++ apps/fetch/tests.mk
@@ -6,12 +6,15 @@
 #TESTS += $(call test-name,$(shell echo 1024*32|bc),$(shell echo 1024|bc))
 #TESTS += $(call test-name,$(shell echo 16*1024*32|bc),$(shell echo 16*1024|bc))
 #TESTS += $(call test-name,$(shell echo 1024*1024*32|bc),$(shell echo 1024*1024|bc))
-TESTS += $(call test-name,$(shell echo 1024*1024*1024|bc),$(shell echo  2*1024*1024|bc))
-TESTS += $(call test-name,$(shell echo 1024*1024*1024|bc),$(shell echo  4*1024*1024|bc))
-TESTS += $(call test-name,$(shell echo 1024*1024*1024|bc),$(shell echo  8*1024*1024|bc))
-TESTS += $(call test-name,$(shell echo 1024*1024*1024|bc),$(shell echo 16*1024*1024|bc))
-TESTS += $(call test-name,$(shell echo 1024*1024*1024|bc),$(shell echo 32*1024*1024|bc))
-TESTS += $(call test-name,$(shell echo 1024*1024*1024|bc),$(shell echo 64*1024*1024|bc))
+TESTS += $(call test-name,$(shell echo 1024*1024*1024|bc),$(shell echo   2*1024*1024|bc))
+TESTS += $(call test-name,$(shell echo 1024*1024*1024|bc),$(shell echo   4*1024*1024|bc))
+TESTS += $(call test-name,$(shell echo 1024*1024*1024|bc),$(shell echo   8*1024*1024|bc))
+TESTS += $(call test-name,$(shell echo 1024*1024*1024|bc),$(shell echo  16*1024*1024|bc))
+TESTS += $(call test-name,$(shell echo 1024*1024*1024|bc),$(shell echo  32*1024*1024|bc))
+TESTS += $(call test-name,$(shell echo 1024*1024*1024|bc),$(shell echo  64*1024*1024|bc))
+TESTS += $(call test-name,$(shell echo 1024*1024*1024|bc),$(shell echo 128*1024*1024|bc))
+TESTS += $(call test-name,$(shell echo 1024*1024*1024|bc),$(shell echo 256*1024*1024|bc))
+
 
 
 
